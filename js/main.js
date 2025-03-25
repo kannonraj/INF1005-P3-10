d803e6996528aa1285a6e0dc5a2dbcad7c7c9622
kannan <kannanrajj98@gmail.com>
2025-03-25 16:38:57 +0800

    Remove unused dropdown initialization code from main.js

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     var dropdownElements = document.querySelectorAll('.dropdown-toggle');
-//     dropdownElements.forEach(function (dropdown) {
-//         new bootstrap.Dropdown(dropdown);
-//     });
-// });
