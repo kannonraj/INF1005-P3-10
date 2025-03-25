6a73f33ed725119c5e6b688b3e84d9a160f407e6
kannan <kannanrajj98@gmail.com>
2025-03-25 14:42:25 +0800

    Remove commented-out dropdown initialization code from main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var dropdownElements = document.querySelectorAll('.dropdown-toggle');
-    dropdownElements.forEach(function (dropdown) {
-        new bootstrap.Dropdown(dropdown);
-    });
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//     var dropdownElements = document.querySelectorAll('.dropdown-toggle');
+//     dropdownElements.forEach(function (dropdown) {
+//         new bootstrap.Dropdown(dropdown);
+//     });
+// });
