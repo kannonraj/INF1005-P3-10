1af5db17845d9389da404b7676d3efd579ca6eb6
TO-Bryce <2401073@sit.singaporetech.edu.sg>
2025-03-26 10:53:29 +0800

    Update Color for Nav/Footer
    css

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
         <!-- Logo on the left -->
         <a class="navbar-brand" href="index.php">
